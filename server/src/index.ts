375acabfb65076b93c33e7da5532ba35876fb21a
Rahul kardile <rahulkardile321@gmail.com>
2024-09-19 19:51:01 +0530

    error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction, Express } from 'express';
+import CustomError from './types/CustomError';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3300;
@@ -6,6 +7,22 @@ const PORT = process.env.PORT || 3300;
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello, TypeScript with Express!');
 });
+
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction)=>{
+    try {
+        
+        let message = err.message || "Internal server error";
+        let status = err?.statusCode || 500;
+        
+        res.status(status).json({
+            success: false,
+            message
+        })
+
+    } catch (error) {
+        next(error)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
