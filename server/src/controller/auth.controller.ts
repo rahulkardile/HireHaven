9bf61238a5653cc92c8be443170a75bc239bd6b8
Rahul kardile <rahulkardile321@gmail.com>
2024-09-19 20:02:33 +0530

    routes is added

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export default class Auth {
     static async login(req: Request, res: Response, next: NextFunction){
         try {
+            const { } = req.body;
             
         } catch (error) {
             next(error);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction, Express } from 'express';
 import CustomError from './types/CustomError';
+import router from './routes/routes';
 
 const app: Express = express();
 const PORT = process.env.PORT || 3300;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3300;
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello, TypeScript with Express!');
 });
+
+app.use("/api", router);
 
 app.use((err: CustomError, req: Request, res: Response, next: NextFunction)=>{
     try {
